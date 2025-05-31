f97c2921910671f955825a238b9d09bbfa9426b1
Oummy100 <mamoudousouleyh98@gmail.com>
2025-05-31 11:01:05 +0100

    C03

---
+++ fibonacci_series.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int limite;
+    int a = 0, b = 1, suivant;
+
+    // Lire la valeur limite
+    printf("Entrez une limite : ");
+    scanf("%d", &limite);
+
+    printf("Fibonacci : ");
+
+    // Afficher les deux premiers termes
+    printf("%d %d ", a, b);
+
+    suivant = a + b;
+
+    while (suivant <= limite) {
+        printf("%d ", suivant);
+        a = b;
+        b = suivant;
+        suivant = a + b;
+    }
+
+    printf("\n");
+    return 0;
+}
